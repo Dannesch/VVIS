bd37cf4538885da6a2a959a1892e63ea51508c62
Danne_sch <35847188+Dannesch@users.noreply.github.com>
2022-03-10 12:31:56 +0100

    updated screenshot function

--- VVIS with mail - Copy.py
+++ VVIS with mail - Copy.py
@@ -1,29 +1,42 @@
-from __future__ import print_function
-from flask import Flask, render_template, request
-import webbrowser
 import email, smtplib, ssl
 import os
-import pyautogui 
 import time
 import pytz
 import os.path
+from playwright.sync_api import sync_playwright
+from flask import Flask, render_template, request
 from datetime import datetime
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-web_site = Flask(__name__)
-web_site.debug = True
+web = Flask(__name__)
+web.debug = True
 sender_email = "dsanne75@gmail.com"
 password = "rnrjdiiwsojxmqcg"
 
-places_def = ["Karta", "Soderhall", "Sattra", "Sono", "Glugga", "Aby", "Vaddo", "Alunda", "Halkriskkarta"]
-web_def = ["https://vvis.trafikverket.se/","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=224&RetX=688646&RetY=6619193", "https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=309&RetX=694731&RetY=6661526","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=238&RetX=702669&RetY=6645651","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=308&RetX=677004&RetY=6643005","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=229&RetX=671448&RetY=6627659","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=239&RetX=711061&RetY=6656764","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=306&RetX=668992&RetY=6661526","https://vvis.trafikverket.se/Functions/Slip/Slipmap.aspx"]
-pyautogui.FAILSAFE = True
+names = ["Karta", "Soderhall", "Sattra", "Sono", "Glugga", "Aby", "Vaddo", "Alunda", "Halkriskkarta"]
+urls = ["https://vvis.trafikverket.se/","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=224&RetX=688646&RetY=6619193", "https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=309&RetX=694731&RetY=6661526","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=238&RetX=702669&RetY=6645651","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=308&RetX=677004&RetY=6643005","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=229&RetX=671448&RetY=6627659","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=239&RetX=711061&RetY=6656764","https://vvis.trafikverket.se/Functions/StationInfo/StationInfoPopup.aspx?Railroad=false&Term=yttempftyta&StationId=306&RetX=668992&RetY=6661526","https://vvis.trafikverket.se/Functions/Slip/Slipmap.aspx"]
 
-def send(name, subject, places, web):
-    vvis_start = True
+def screenshoter():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        for i in range(len(urls)):
+            page.goto(urls[i])
+            if i == 0:
+                page.locator("input[name=\"ctl00\\$ContentPlaceHolder1\\$VViSLoginControl\\$tabsAccountLogin\\$tabLogin\\$ucLoginTo\\$txtUserName\"]").click()
+                page.locator("input[name=\"ctl00\\$ContentPlaceHolder1\\$VViSLoginControl\\$tabsAccountLogin\\$tabLogin\\$ucLoginTo\\$txtUserName\"]").fill("brian.schembri@svevia.se")
+                page.locator("input[name=\"ctl00\\$ContentPlaceHolder1\\$VViSLoginControl\\$tabsAccountLogin\\$tabLogin\\$ucLoginTo\\$txtUserName\"]").press("Tab")
+                page.locator("input[name=\"ctl00\\$ContentPlaceHolder1\\$VViSLoginControl\\$tabsAccountLogin\\$tabLogin\\$ucLoginTo\\$txtPassWord1\"]").fill("Vagen22")
+                with page.expect_navigation():
+                    page.locator("input[name=\"ctl00\\$ContentPlaceHolder1\\$VViSLoginControl\\$tabsAccountLogin\\$tabLogin\\$ucLoginTo\\$txtPassWord1\"]").press("Enter")
+            time.sleep(3)
+            page.screenshot(path=names[i]+".png")
+        browser.close()
+
+def send(name, subject):
     body = f"{name}"
     receiver_email = "do.norraroslagen@svevia.se"
     if (name.lower() == "test"):
@@ -34,34 +47,10 @@ def send(name, subject, places, web):
     message["Subject"] = subject
     message.attach(MIMEText(body, "plain"))
 
-    #Screenshots
-    for i in range(len(places)):
-        webbrowser.open(f'{web[i]}')
-        if (i == 0):
-            time.sleep(1)
-            pyautogui.press('f11')
-            time.sleep(8)
-            while vvis_start:
-                try:
-                    place = pyautogui.locateOnScreen('loggin.png')
-                    w=place[2]/2
-                    h=place[3]/2
-                    x=place[0]+w
-                    y=place[1]+h
-                    vvis_start = False
-                except:
-                    pass
-            time.sleep(2)
-            pyautogui.moveTo(x, y, duration=0)
-            time.sleep(2)
-            pyautogui.click()
-        time.sleep(15)
-        pyautogui.screenshot(f"{places[i]}.png")
-        time.sleep(2)
-    pyautogui.hotkey('alt', 'f4')
+    screenshoter()
 
     #Attatching images to mail
-    for place in places:
+    for place in names:
         filename = f"{place}.png"
         with open(filename, "rb") as attachment:
             part = MIMEBase("application", "octet-stream")
@@ -82,29 +71,21 @@ def send(name, subject, places, web):
         print(f"sent to: {name}")
     
     #Delete images
-    for i in places:
+    for i in names:
         if(os.path.isfile(f"{i}.png") == True):
             os.remove(f"{i}.png")
 
-
-@web_site.route('/',methods = ['GET','POST'])
+@web.route('/',methods = ['GET','POST'])
 def index():
     if request.method == 'POST':
-        age = request.form['age']
-        if age != "":
-            #t = []
-            #s = request.form.getlist('Place')
-            #print(web_def)
-            #for i in places_def:
-            #    if(i in s):
-            #        c = places_def.index(i)
-            #        t.append(web_def[c])
-            tz = pytz.timezone('Europe/Stockholm')
-            now = datetime.now(tz)
-            date = now.strftime("%y%m%d")
-            tm = now.strftime("%H%M")
-            print(f"Recived from: {age} at {date} kl {tm}")
-            send(age, f"VVIS {date} kl {tm}", places_def, web_def)
+        name = request.form['name']
+        if name != "":
+            time_zone = pytz.timezone('Europe/Stockholm')
+            time = datetime.now(time_zone)
+            current_date = time.strftime("%y%m%d")
+            current_time = time.strftime("%H%M")
+            print(f"Recived from: {name} at {current_date} kl {current_time}")
+            send(name, f"VVIS {current_date} kl {current_time}")
     return render_template('index.html')
 
-web_site.run(host='0.0.0.0', port=8080)
+web.run(host='0.0.0.0', port=8080)
