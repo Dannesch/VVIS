bc02461b629e2103f2d70dd474147714291fce91
Daniel Schembri <dschem.ds@gmail.com>
2022-10-30 21:13:05 +0100

    progress

--- mail_test.py
+++ mail_test.py
@@ -1,5 +1,5 @@
 import requests, time
-from imap_tools import MailBox
+from imap_tools import MailBox, AND
 
 imap_port=993
 mail_server="mail.schemsv.com"
@@ -9,23 +9,24 @@ sender="vvis@schemsv.com"
 def remove_old(new, old):
     for i in old:
         if i in new:
-            del new[i]
+            new.remove(i)
     return new
+
 while True:
     with MailBox(mail_server).login(sender, password, 'INBOX') as mailbox:
-        bodies = {}
         old_len = None
         old_list = None
         for i in range(91):
+            bodies = []
             for msg in mailbox.fetch():
-                if msg.from_ != "bschem67@gmail.com": continue
-                bodies[msg.uid] = {
-                    "text": msg.text,
-                    "subject": msg.subject
-                }
+                if msg.from_ != "dschem.ds@gmail.com": continue
+                bodies.append(msg.uid)
             if old_len != None and old_list != None and old_len < len(bodies):
                 new_messages = remove_old(bodies, old_list)
-            print(bodies)
+                for i in new_messages:
+                    message = list(mailbox.fetch(AND(uid=i)))[0]
+                    print(message.subject)
             old_len = len(bodies)
             old_list = bodies
-            time.sleep(20)
+            print(old_len)
+            time.sleep(2)
